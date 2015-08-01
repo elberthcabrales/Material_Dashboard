13c8b9d296f524317941e49b2f9ee0dd5786285f
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-08-01 21:05:04 +0530

    edits js

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+/*jQuery(document).ready(function($) {
+    Nprogress.start();
+	$(window).load(function (event) {
+        event.preventDefault();
+      	//Nprogress.done();
+    });
+});*/
