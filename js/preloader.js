d29eec2e622304016ac778251a66becb9823260e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-08-01 21:12:24 +0530

    Spinner for NProgress suppressed

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 jQuery(document).ready(function($) {
 	NProgress.configure({ easing: 'ease', speed: 1000 });
+	NProgress.configure({ showSpinner: false });
     NProgress.start();
 	$(window).load(function (event) {
         event.preventDefault();
