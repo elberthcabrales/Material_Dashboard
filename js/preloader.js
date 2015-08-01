0e939c937972519b65ae5736d3856c9c1846398e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-08-01 21:10:14 +0530

    NProgress final

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,9 @@
 // JavaScript Document
-/*jQuery(document).ready(function($) {
-    Nprogress.start();
+jQuery(document).ready(function($) {
+	NProgress.configure({ easing: 'ease', speed: 1000 });
+    NProgress.start();
 	$(window).load(function (event) {
         event.preventDefault();
-      	//Nprogress.done();
+      	NProgress.done();
     });
-});*/
+});
