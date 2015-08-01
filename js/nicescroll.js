6fcdfa613e79d9a4423b6d805977df1d2a5e57d1
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-08-01 20:23:45 +0530

    dashboard plugin updated

--- js/nicescroll.js
+++ js/nicescroll.js
@@ -5,10 +5,3 @@ $(document).ready(function() {
 			cursorborder: "1px solid #757575"
 			});
 }); 
-
-$(document).ready(function() {  
-        $("#content").niceScroll({
-			cursorcolor:"#757575",
-			cursorborder: "1px solid #757575"
-			});
-}); 
