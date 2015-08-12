423068c0f29052ac9fa4f5ed9860552f818a2e40
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-08-12 12:31:56 +0530

    modal updates

--- js/nicescroll.js
+++ js/nicescroll.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
 			cursorborder: "1px solid #757575"
 			});
 }); 
-/************************************/
+/****************Modal********************/
+$(document).ready(function() {  
+        $("#add_modal").niceScroll({
+			cursorcolor:"#757575",
+			cursorborder: "1px solid #757575"
+			});
+});
